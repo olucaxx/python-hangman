2b408cfd918f81e11a17402422a349532d0b3756
Lucas Pereira <devpereiralucas@gmail.com>
2025-02-16 19:42:18 -0300

    bug fixes e add selecionar palavra do arquivo txt

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,5 +1,12 @@
 import random
 
+def select_random_word():
+    """seleciona uma palavra aleatória do arquivo words.txt e retorna ela"""
+    with open('hangman/words.txt') as file:
+        words = file.readlines()
+        
+    return random.choice(words).strip()
+
 def display_list(list):
     """imprime todos os caracteres de uma lista separados por espaços no console"""
     for char in list:
@@ -16,9 +23,9 @@ def compare_guess(secret_word, hint, guess):
     """compara o chute com a palavra secreta, atualizando as letras acertadas"""
     correct_guess = False
     for i in range(len(secret_word)):
-            if secret_word[i] == guess:
-                hint[i] = guess
-                correct_guess = True
+        if secret_word[i] == guess:
+            hint[i] = guess
+            correct_guess = True
     return correct_guess
 
 
@@ -27,24 +34,24 @@ def check_guess(guess, hint, secret_word, guessed_letters):
     if len(guess) > 1:
         if len(guess) != len(secret_word):   
             return False, False
-        if secret_word == list(guess):
+        if secret_word == guess:
+            hint[:] = list(secret_word)
             return True, True
-        print(secret_word, guess)
         return False, False
 
     if guess not in guessed_letters:
         guessed_letters.add(guess)
-        return compare_guess(secret_word, hint, guess), secret_word == hint
-        
+        correct_guess = compare_guess(secret_word, hint, guess)
+        return correct_guess, hint == list(secret_word)
+    
     print("you already guessed this letter! try again.")
     return True, False
 
 
 def main():
     """executa o código principal para o jogo"""
-    words = ("phone", "internet", "python")
-
-    secret_word = list(random.choice(words))
+    secret_word = select_random_word()
+    print(f"'{secret_word}'")
     hint = ["_" for x in secret_word]
     guessed_letters = set()
 
@@ -61,6 +68,7 @@ def main():
             
     if not has_won:
         print("you lost, better luck next time")
+        print(f"the word was {secret_word}")
         return
     
     display_list(secret_word)
